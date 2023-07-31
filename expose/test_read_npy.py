95494c0e20c2134adb470c98b9e4300a1df54991
JYe16 <54547626+JYe16@users.noreply.github.com>
2023-07-31 15:31:38 -0400

    change

--- expose/test_read_npy.py
+++ expose/test_read_npy.py
@@ -2,5 +2,6 @@ import numpy as np
 import torch
 
 if __name__ == '__main__':
+    npz = np.load('/mnt/h/Datasets/Own/img/0606-yzk-play-phone1/test/0000.jpg_000_params.npz')
     tt = torch.load('/mnt/d/yzk/NTU/base_joint_pt_single/train/S001C001P001R001A002_base_joint.pt')
     print(tt)
