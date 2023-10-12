73b96955244c0d0369a9579d5aa55cde18553126
JYe16 <54547626+JYe16@users.noreply.github.com>
2023-10-12 09:28:25 -0400

    Update smplx_convert.sh

--- expose/smplx_convert.sh
+++ expose/smplx_convert.sh
@@ -1,13 +1,20 @@
 conda activate expose
 
-for d in /mnt/h/Datasets/NTU/rgb_img_single/train/*;
-do
-	python rgb2pt.py --img-folder $d --show=False --save-mesh False --save-params True --output-folder /mnt/h/Datasets/NTU/params_single/
-done
+# for d in /mnt/h/Datasets/NTU/rgb_img_single/train/*;
+# do
+# 	python rgb2pt.py --img-folder $d --show=False --save-mesh False --save-params True --output-folder /mnt/h/Datasets/NTU/params_single/
+# done
 
 for d in /mnt/h/Datasets/NTU/rgb_img_single/test/*;
 do
-	python rgb2pt.py --img-folder $d --show=False --save-mesh False --save-params True --output-folder /mnt/h/Datasets/NTU/params_single/
+	file="/mnt/h/Datasets/NTU/params_single/test/"${d##*/}".pt"
+	# echo "$file"
+	if [ -e "$file" ]
+	then
+		echo "File exists"
+	else
+		python rgb2pt.py --img-folder $d --show=False --save-mesh False --save-params True --output-folder /mnt/h/Datasets/NTU/params_single/
+	fi
 done
 
 conda deactivate
